89aeb88c57ac16d5ed46530e57caa7a49cd123bd
itslastwxxk <vladdomg@icloud.com>
2026-09-22 21:16:10 +0300

    1

--- bot.py
+++ bot.py
@@ -2429,10 +2429,10 @@ async def show_work_menu(message: Message, state: FSMContext):
     text = f"<b>выбирай, где хочешь работать:</b>"
     try:
         photo = FSInputFile("images/work.png")
-        await message.answer_photo(photo=photo, caption=text, reply_markup=get_work_keyboard())
+        await message.answer_photo(photo=photo, caption=text, parse_mode="HTML", reply_markup=get_work_keyboard())
     except FileNotFoundError:
         logger.warning("Файл images/work.png не найден.")
-        await message.answer(text, parse_mode="HTML", reply_markup=get_work_keyboard())
+        await message.answer(text, reply_markup=get_work_keyboard())
 
 @router.message(F.text == "🛒 Магаз")
 async def show_shop_menu(message: Message):
